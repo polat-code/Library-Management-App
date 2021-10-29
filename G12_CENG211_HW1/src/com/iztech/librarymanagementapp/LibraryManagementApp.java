5784f59476d53fcf49593619b15ad56fc3b66662
Burak Keçeci <bburakkececi@gmail.com>
2021-10-29 09:56:52 +0300

    update

--- G12_CENG211_HW1/src/com/iztech/librarymanagementapp/LibraryManagementApp.java
+++ G12_CENG211_HW1/src/com/iztech/librarymanagementapp/LibraryManagementApp.java
@@ -1,14 +1,17 @@
 package com.iztech.librarymanagementapp;
 
 public class LibraryManagementApp {
-  
-  public static void main (String[] args) {
-    
-    LibraryQuery query = new LibraryQuery();
+
+	public static void main(String[] args) {
+
+		LibraryQuery query = new LibraryQuery();
+		
 		query.getMaxIssuedBook();
 		query.getMemberMostIssue();
 		query.getHighPenalty();
 		query.getMostCopy();
-  }
+		query.getFewestCopyIssuedBooks();
+		query.getMemberLeastIssue();
+	}
 
 }
